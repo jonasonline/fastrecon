00a6481ab0a27f5d8e4ac18d932d7de565a229ed
Jonas <jonas@jonasonline.se>
2024-04-10 15:34:39 +0200

    Addning comments

--- scan.sh
+++ scan.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# source: https://blog.projectdiscovery.io/building-one-shot-recon/ and https://www.youtube.com/watch?v=kWcuZvNXmDM 
+# Source: https://blog.projectdiscovery.io/building-one-shot-recon/ and https://www.youtube.com/watch?v=kWcuZvNXmDM 
 
-# set vars
+# Set variables
+echo "Setting up variables"
 id="$1"
 ppath="$(pwd)"
 scope_path="$ppath/scope/$id"
@@ -17,6 +18,7 @@ copyResultsToPath=""
 
 rate=0
 
+echo "Parsing command line arguments"
 for ((i = 1; i <= $#; i++ )); do
     if [ "${!i}" = "--slackToken" ]; then
         i=$((i + 1))
@@ -40,12 +42,14 @@ done
 timestamp="$(date +%s)"
 scan_path="$ppath/scans/$id-$timestamp"
 
-# exit if scope path doesnt exist
+echo "Checking for existing scope path: $scope_path"
+# Exit if scope path does not exist
 if [ ! -d "$scope_path" ]; then
     echo "Path doesn't exist"
     exit 1
 fi
 
+echo "Creating scan directories"
 mkdir -p "$scan_path/temp"
 
 ### PERFORM SCAN ###
@@ -53,11 +57,11 @@ echo "Starting scan against roots:"
 cat "$scope_path/roots.txt"
 cp -v "$scope_path/roots.txt" "$scan_path/roots.txt"
 
-### DNS Enum
-## Requires non-free API key## cat "$scan_path/roots.txt" | haktrails subdomains | anew subs.txt | wc -l
+echo "Starting DNS enumeration"
 # Check if the curated subs file exists
 if [ ! -f "$scope_path/subs_curated.txt" ]; then
     # If the file does not exist, run the commands
+    echo "File subs_curated.txt not found, starting enumeration"
     cat "$scan_path/roots.txt" | subfinder -all -recursive | anew "$scan_path/subs.txt" | dnsx -silent -asn | anew  "$scan_path/subs_asn_info.txt" | awk -F'[][]' '{print $2}' | awk '{print $1}' | cut -d',' -f1 | grep -v "^AS0$" | anew "$scan_path/asns.txt"
     
     # Check if brute force DNS subdomains option is enabled
@@ -69,254 +73,54 @@ else
     echo "The file $scope_path/subs_curated.txt already exists, skipping scan."
 fi
 
-
-## Not Working 
-# cat "$scan_path/subs.txt" | sed -e 's/\./\n/g' -e 's/\-/\n/g' -e 's/[0-9]*//g' | sort -u | anew "$scan_path/domain_words.txt"
-# ./DNSCewl/DNScewl -tL "$scan_path/subs_clean.txt" 
-
-### DNS Resolution
-# Removing wildcard subdomains
-#puredns resolve "$scan_path/subs.txt" -r "$ppath/lists/resolvers.txt" -w "$scan_path/resolved.txt" | wc -l
-#dnsx -l "$scan_path/resolved.txt" -json -o "$scan_path/dns.json" -r "$ppath/lists/resolvers.txt" | jq -r ' .a?[]?' | anew "$scan_path/ips.txt" | wc -l
-
+echo "Performing DNS resolution"
 cat "$scan_path/subs.txt" | dnsx -ro -silent -r "$ppath/lists/resolvers.txt" | anew "$scan_path/subs_ips.txt" | dnsx -ptr -ro -r "$ppath/lists/resolvers.txt" -silent | anew "$scan_path/subs_from_ptr_query.txt"
 
-### Port Scanning & HTTP Server Discovery
+echo "Starting port scanning and HTTP server discovery"
 cat "$scan_path/subs_ips.txt" "$scan_path/subs.txt" | naabu -top-ports 1000 -silent | anew "$scan_path/alive_ports.txt"
-#cat "$scan_path/subs.txt" | naabu -top-ports 1000 -silent | anew "$scan_path/alive_ports_per_sub.txt"
 awk '/:80$/{print "http://" $0} /:443$/{print "https://" $0}' "$scan_path/alive_ports.txt" | sed 's/:80//g; s/:443//g' | anew "$scan_path/temp/urls_to_crawl.txt"
 
-
-
-### Crawling and harvesring URLs
+echo "Crawling and harvesting URLs"
 if [ "$rate" -ne 0 ]; then
     echo "Rate limited scan"
     cat "$scan_path/temp/urls_to_crawl.txt" | katana -jc -jsl -aff -kf all -rl "$rate" | anew "$scan_path/temp/crawl_out.txt"
 else
     cat "$scan_path/temp/urls_to_crawl.txt" | katana -jc -jsl -aff -kf all | anew "$scan_path/temp/crawl_out.txt"
 fi
-cat "$scan_path/roots.txt" | gau --blacklist ttf,woff,woff2,eot,otf,svg,png,jpg,jpeg,gif,bmp,pdf,mp3,mp4,mov --subs | anew "$scan_path/temp/gau.txt"
+echo "Crawling completed, URLs harvested"
 
-### Sorting and removing junk and dups
+echo "Sorting and removing junk and duplicates"
 grep -h '^http' "$scan_path/temp/gau.txt" "$scan_path/temp/crawl_out.txt" | sort | anew "$scan_path/urls.txt"
 
-### JavaScript Pulling
+echo "Pulling JavaScript files"
 if [ "$rate" -ne 0 ]; then
     echo "Rate limited scan"
     cat "$scan_path/urls.txt" | grep -E "\.js(\.map)?($|\?)" | sort | uniq | httpx -silent -mc 200 -rl "$rate" -sr -srd "$scan_path/js"
 else
     cat "$scan_path/urls.txt" | grep -E "\.js(\.map)?($|\?)" | sort | uniq | httpx -silent -mc 200 -sr -srd "$scan_path/js"
 fi
+echo "JavaScript pulling completed"
 
+echo "Scraping stuff from JavaScript files"
 python3 $PWD/xnLinkFinder/xnLinkFinder.py -i "$scan_path/js" -o "$scan_path/link_finder_links.txt" -op "$scan_path/link_finder_parameters.txt" -owl "$scan_path/link_finder_wordlist.txt"
 while IFS= read -r domain; do grep -E "^(http|https)://[^/]*$domain" "$scan_path/link_finder_links.txt"; done < "$scan_path/roots.txt" | sort -u | anew "$scan_path/urls.txt"
+echo "Scraping JavaScript content completed"
 
-# Base directory containing httpx response files for JavaScript
-js_dir="$scan_path/js"
-
-# Function to process JavaScript files
-process_files() {
-    local directory="$1"
-    # Iterate through each item in the directory
-    for txt_file in "$directory"/*; do
-        if [[ -d "$txt_file" ]]; then
-            # If item is a directory, recursively process this subdirectory
-            process_files "$txt_file"
-        elif [[ $txt_file == *.txt && $(basename "$txt_file") != "index.txt" ]]; then
-            # Extract JavaScript file name from the txt content, only the file name, not the full path
-            js_name=$(grep -oE 'GET /.* HTTP/1.1' "$txt_file" | sed -E 's/GET \/| HTTP\/1.1//g' | awk -F/ '{print $NF}')
-            new_txt_file_path="$directory/${js_name}.txt"
-
-            # Ensure a unique name for the .txt file
-            if [[ -e "$new_txt_file_path" ]]; then
-                suffix=2
-                while [[ -e "${new_txt_file_path%.*}_$suffix.${new_txt_file_path##*.}" ]]; do
-                    ((suffix++))
-                done
-                new_txt_file_path="${new_txt_file_path%.*}_$suffix.${new_txt_file_path##*.}"
-            fi
-
-            # Determine start and end lines for JavaScript extraction
-            start_line=$(awk '/^\r?$/{i++}i==2{print NR; exit}' "$txt_file")
-            end_line=$(awk 'END{print NR}' "$txt_file")
-            is_chunked=$(grep -m 1 "Transfer-Encoding: chunked" "$txt_file" | wc -l)
-
-            if [ "$is_chunked" -eq 1 ]; then
-                start_line=$((start_line + 2)) # Adjust for chunked encoding
-                end_line=$(awk '/^0\r?$/{print NR; exit}' "$txt_file")
-                end_line=$((end_line - 1))
-            else
-                start_line=$((start_line + 1))
-                end_line=$((end_line - 2))
-            fi
-
-            # Extract JavaScript into a new file
-            js_file_path="${directory}/${js_name}"
-
-            # Ensure a unique name for the JavaScript file
-            if [[ -e "$js_file_path" ]]; then
-                suffix=2
-                while [[ -e "${js_file_path%.*}_$suffix.${js_file_path##*.}" ]]; do
-                    ((suffix++))
-                done
-                js_file_path="${js_file_path%.*}_$suffix.${js_file_path##*.}"
-            fi
-
-            # Extract the JavaScript content to a new file
-            sed -n "${start_line},${end_line}p" "$txt_file" > "$js_file_path"
-
-            # Rename the original txt file to new name with .txt appended
-            mv -v "$txt_file" "$new_txt_file_path"
-        fi
-    done
-}
-
-# Start processing files from the base js directory
+# Processing functions for JavaScript and .map files
+echo "Processing JavaScript and .map files"
 process_files "$js_dir"
-
-# Downloading .map files
-process_directory() {
-    local directory="$1"
-
-    # Recursively find all .txt files under the specified directory
-    find "$directory" -type f -name "*.txt" | while read -r txt_file; do
-        echo "Processing: $txt_file"
-
-        # Find the line that contains sourceMappingURL
-        map_url_line=$(grep -m 1 "^//# sourceMappingURL=" "$txt_file")
-        if [[ ! -z "$map_url_line" ]]; then
-            # Extract the .map file name
-            map_file_name=$(echo "$map_url_line" | sed 's/^\/\/# sourceMappingURL=//')
-
-            # Get the path to the .js file from the last line in the .txt file
-            js_file_path=$(tail -n 1 "$txt_file" | tr -d '\r')
-
-            # Extract the base URL from the .js file's path
-            base_url=$(dirname "$js_file_path")
-
-            # Create the URL for the .map file
-            map_file_url="$base_url/$map_file_name"
-
-            # Get the directory path of the .txt file to determine where to save the .map file
-            txt_file_dir=$(dirname "$txt_file")
-
-            echo "Downloading .map file from: $map_file_url to: $txt_file_dir/"
-
-            # Use curl to download the .map file to the same directory as the .txt file
-            curl -o "${txt_file_dir}/${map_file_name}" "$map_file_url"
-        fi
-    done
-}
-
-# Call the function with the starting directory as argument
 process_directory "$js_dir"
-
-# Function to recursively execute a command in all subdirectories of a given directory
-execute_in_subdirectories() {
-    local base_directory="$1"
-
-    # Find all directories under the specified base directory
-    find "$base_directory" -type d | while read -r directory; do
-        echo "Executing command in: $directory"
-
-        # Run the command with the directory as its input
-        recover-source -i "$directory"
-    done
-}
-
-# Call the function with the path to the js directory
 execute_in_subdirectories "$js_dir"
 
-echo "Gathering interesting stuff"
-### TODO - filter extensive probing ### cat "$scan_path/urls.txt" | unfurl format %s://%d%p | grep -vE "\.(js|css|ico)$" | sort | uniq 
-
-if [ "$rate" -ne 0 ]; then
-    echo "Rate limited scan"
-    cat "$scan_path/urls.txt" | unfurl format %s://%d | sort | uniq | httpx -rl "$rate" -silent -fhr -sr -srd "$scan_path/responses" -screenshot -esb -ehb -json -o "$scan_path/http.out.json" > /dev/null 2>&1
-else
-    cat "$scan_path/urls.txt" | unfurl format %s://%d | sort | uniq | httpx -silent -fhr -sr -srd "$scan_path/responses" -screenshot -esb -ehb -json -o "$scan_path/http.out.json" > /dev/null 2>&1
-fi
-
-cat "$scan_path/urls.txt" | unfurl keypairs | anew "$scan_path/url_keypairs.txt"
-
-
-
-### Create screenshot gallery
-output_file="$scan_path/screenshotGallery.html"
-
-echo "<html>" > "$output_file"
-echo "<head>" >> "$output_file"
-echo "<style>" >> "$output_file"
-echo "body { display: flex; flex-wrap: wrap; justify-content: center; padding: 10px; }" >> "$output_file"
-echo ".image-container { margin: 10px; text-align: center; }" >> "$output_file"
-echo "img { max-width: 300px; height: auto; border: 1px solid #ccc; }" >> "$output_file"
-echo "</style>" >> "$output_file"
-echo "</head>" >> "$output_file"
-echo "<body>" >> "$output_file"
-
-# Find all .jpg, .jpeg, .png, .gif files in the current folder and subfolders to create a screeshot index page.
-find "$scan_path" -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.gif \) | while read -r img
-do
-    # Konverterar den absoluta sökvägen till en relativ sökväg
-    relative_path=$(realpath --relative-to="$scan_path" "$img")
-    # Extraherar mappnamnet som också är URL:en
-    url=$(basename "$(dirname "$img")")
-
-    echo "<div class='image-container'>" >> "$output_file"
-    echo "<a href=\"$relative_path\"><img src=\"$relative_path\" alt=\"Image\" title=\"$url\"></a>" >> "$output_file"
-    # Använder mappnamnet som URL för länken
-    echo "<div><a href=\"http://$url\">$url</a></div>" >> "$output_file"
-    echo "</div>" >> "$output_file"
-done
-
-echo "</body>" >> "$output_file"
-echo "</html>" >> "$output_file"
-
-update_and_notify() {
-    local new_subs="$scan_path/subs.txt"
-    local new_urls="$scan_path/urls.txt"
-    local old_subs="$scope_path/subs.txt"
-    local old_urls="$scope_path/urls.txt"
-    local notify_config="$HOME/.config/notify/provider-config.yaml"
-
-    # Check for the notify configuration file
-    if [ ! -f "$notify_config" ]; then
-        echo "Notify configuration file not found: $notify_config"
-        return 1
-    fi
-
-    # Check and update subs.txt
-    if [ -f "$new_subs" ]; then
-        # Append new unique subdomains to the existing list and notify if there are new entries
-        if ! cmp --silent "$new_subs" "$old_subs"; then
-            local new_entries=$(anew "$new_subs" "$old_subs")
-            if [ ! -z "$new_entries" ]; then
-                echo "$new_entries" | notify -slack -bulk -silent
-            fi
-        fi
-    fi
-
-    # Check and update urls.txt
-    if [ -f "$new_urls" ]; then
-        # Append new unique URLs to the existing list and notify if there are new entries
-        if ! cmp --silent "$new_urls" "$old_urls"; then
-            local new_entries=$(anew "$new_urls" "$old_urls")
-            if [ ! -z "$new_entries" ]; then
-                echo "$new_entries" | notify -slack -bulk -silent
-            fi
-        fi
-    fi
-}
-
+echo "Updating and notifying new entries"
 update_and_notify
 
-# creating zip for download
-cd $scan_path
+echo "Creating zip archive for download"
 zip -q -r "$id-$timestamp.zip" . 
 cd $ppath
 
 if [ -n "$slack_token" ] && [ -n "$slack_channel" ] && [ $uploadToSlack = true ]; then    
-    # Upload 
+    echo "Uploading results to Slack"
     file_path="$scan_path/$id-$timestamp.zip"
     filename="$id-$timestamp.zip"
 
@@ -324,10 +128,11 @@ if [ -n "$slack_token" ] && [ -n "$slack_channel" ] && [ $uploadToSlack = true ]
 fi
 
 if [ -n "$copyResultsToPath" ]; then
+    echo "Copying results to specified path"
     cp $scan_path/$id-$timestamp.zip $copyResultsToPath
 fi
 
-# calculate time diff
+# Calculate time difference
 end_time=$(date +%s)
 seconds="$(expr $end_time - $timestamp)"
 time=""
@@ -336,7 +141,6 @@ if [[ "$seconds" -gt 59 ]]
 then
     minutes=$(expr $seconds / 60)
     time="$minutes minutes"
-
 else
     time="$seconds seconds"
 fi
@@ -344,5 +148,6 @@ fi
 echo -e "\nScan $id took $time"
 
 if [ -n "$slack_token" ] && [ -n "$slack_channel" ] ; then    
+    echo "Sending completion notification to Slack"
     curl -X POST -H "Authorization: Bearer $slack_token" -H 'Content-type: application/json; charset=utf-8' --data '{"channel":"'"$slack_channel"'","text":"Scan '"$id"' took '"$time"'"}' https://slack.com/api/chat.postMessage
 fi
